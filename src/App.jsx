cff9b56dcd7a3cce308051c17a4c9142bc565c45
Romanna <romannabric@gmail.com>
2026-05-03 10:22:26 +0200

    feat: add React Routee

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import TeachersPage from "./pages/TeachersPage/TeachersPage";
+import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/teachers" element={<TeachersPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
