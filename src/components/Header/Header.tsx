410966cdea7860d80c0624e864c7eb28d8c9e623
Romanna <romannabric@gmail.com>
2026-05-04 22:17:38 +0200

    feat: add header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,58 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 import css from "./Header.module.css";
 
 const Header = () => {
+  const { isLoggedIn, user, logout } = useAuth();
+
   return (
     <header className={css.header}>
-      <NavLink to="/" className={css.logo}>
-        LearnLingo
-      </NavLink>
+      <div className="container">
+        <div className={css.inner}>
+          <NavLink to="/" className={css.logo}>
+            <svg className={css.logoIcon}>
+              <use href="/sprite.svg#icon-logo" />
+            </svg>
+            LearnLingo
+          </NavLink>
 
-      <nav className={css.nav}>
-        <NavLink to="/" className={css.link}>
-          Home
-        </NavLink>
-        <NavLink to="/teachers" className={css.link}>
-          Teachers
-        </NavLink>
-      </nav>
+          <nav className={css.nav}>
+            <NavLink to="/" className={css.link}>
+              Home
+            </NavLink>
+            <NavLink to="/teachers" className={css.link}>
+              Teachers
+            </NavLink>
+            {isLoggedIn && (
+              <NavLink to="/favorites" className={css.link}>
+                Favorites
+              </NavLink>
+            )}
+          </nav>
 
-      <div className={css.auth}>
-        <button type="button" className={css.loginBtn}>
-          Log in
-        </button>
-        <button type="button" className={css.registerBtn}>
-          Registration
-        </button>
+          <div className={css.auth}>
+            {isLoggedIn ? (
+              <>
+                <span className={css.userEmail}>{user?.email}</span>
+                <button type="button" className={css.loginBtn} onClick={logout}>
+                  Log out
+                </button>
+              </>
+            ) : (
+              <>
+                <button type="button" className={css.loginBtn}>
+                  <svg className={css.loginIcon}>
+                    <use href="/sprite.svg#icon-log-in" />
+                  </svg>
+                  Log in
+                </button>
+                <button type="button" className={css.registerBtn}>
+                  Registration
+                </button>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </header>
   );
